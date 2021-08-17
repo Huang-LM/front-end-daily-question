c8af90dc1f3f82c9baf337f77c3602a89204d12e
Huang-LM <57673015+Huang-LM@users.noreply.github.com>
2021-08-17 15:00:31 +0800

    fix: 删除注释

--- src/command/share.ts
+++ src/command/share.ts
@@ -1,11 +1,5 @@
 import * as vscode from "vscode";
-// import * as tempy from "tempy";
-// import * as queryString from "query-string";
-// import * as execa from "execa";
 import { webViewPanel } from "./shareWeb";
-// const SAVE_DIRECTORY = tempy.directory();
-
-// const FULL_DOWNLOADED_PATH = `${SAVE_DIRECTORY}/carbon.png`;
 
 // 获取页面中代码的方法
 const processContent = (input: string, START = 0, END = 1000) => {
@@ -35,38 +29,6 @@ const processContent = (input: string, START = 0, END = 1000) => {
 	});
 };
 
-// 复制的方法
-// const imgToClipboard = async (imgPath: string) => {
-// 	const OS = process.platform;
-// 	let SCRIPT;
-
-// 	switch (OS) {
-// 		case "darwin": {
-// 			SCRIPT = `osascript -e 'set the clipboard to (read (POSIX file "${imgPath}") as JPEG picture)'`;
-// 			break;
-// 		}
-// 		case "win32": {
-// 			SCRIPT = `nircmd clipboard copyimage ${imgPath}`;
-// 			break;
-// 		}
-// 		default: {
-// 			SCRIPT = `xclip -selection clipboard -t image/png -i ${imgPath}`;
-// 		}
-// 	}
-
-// 	// Running `await execa` leads to `Listr` not resolving the last task on Linux
-// 	// Hence, we need to distinguish between OS’s and run it with or without `await`
-// 	// This solution is not insanely beautiful, but makes it work cross-OS ¯\_(ツ)_/¯
-// 	if (OS === "darwin" || OS === "win32") {
-// 		await execa(SCRIPT, [], {
-// 			shell: true,
-// 		});
-// 	} else {
-// 		execa(SCRIPT, [], {
-// 			shell: true,
-// 		});
-// 	}
-// };
 
 export async function share(
 	content: string,
@@ -75,40 +37,12 @@ export async function share(
 	try {
 		vscode.window.showInformationMessage("正在生成图片，请稍等...");
 		const processedContent = await processContent(content);
-		// const urlEncodedContent = encodeURIComponent(processedContent);
 
 		webViewPanel(context, processedContent);
-		// console.log(url);
 
-		// const settings = {
-		// 	code: urlEncodedContent,
-		// 	l: "auto",
-		// };
-		// let url = "https://carbon.now.sh/";
-		// url = `${url}?${queryString.stringify(settings)}`;
-		// vscode.window.showInformationMessage("正在生成图片，请稍等...");
-		// 生成东西的方法
-		// await headlessVisit({
-		// 	url,
-		// 	location: SAVE_DIRECTORY,
-		// 	type: "png",
-		// 	headless: true,
-		// });
-
-		vscode.window.showInformationMessage("生成成功，请稍等...");
-		// const downloadedAs = FULL_DOWNLOADED_PATH;
-		// await imgToClipboard(downloadedAs);
-		// vscode.window.showInformationMessage(
-		// 	"生成成功，图片已生成到您的剪切板，请粘贴分享！"
-		// );
-		// vscode.window.showInformationMessage(
-		// 	"生成成功，图片已生成到您的剪切板，请粘贴分享！"
-		// );
+		vscode.window.showInformationMessage("生成成功！");
 	} catch (error) {
 		console.log("err", error);
 		vscode.window.showInformationMessage("生成失败！");
 	}
 }
-// function headlessVisit(arg0: { url: string; location: any; type: string; headless: boolean; }) {
-// 	throw new Error("Function not implemented.");
-// }
--- src/command/shareWeb.ts
+++ src/command/shareWeb.ts
@@ -1,12 +1,5 @@
 import * as vscode from "vscode";
 import * as path from "path";
-// import * as tempy from "tempy";
-// import * as fs from "fs";
-// import * as queryString from "query-string";
-// import * as execa from "execa";
-// const SAVE_DIRECTORY = tempy.directory();
-
-// const FULL_DOWNLOADED_PATH = `${SAVE_DIRECTORY}/shareImg.png`;
 
 export function webViewPanel(context: vscode.ExtensionContext, code: string) {
 	// 1. 使用 createWebviewPanel 创建一个 panel，然后给 panel 放入 html 即可展示 web view
@@ -34,35 +27,6 @@ export function webViewPanel(context: vscode.ExtensionContext, code: string) {
 	const URI = onDiskPath.with({ scheme: "vscode-resource" });
 
 	panel.webview.html = getWebviewContent(code, JSON.stringify(template), URI);
-
-	// Handle messages from the webview
-	// panel.webview.onDidReceiveMessage(
-	// 	async (base) => {
-	// 		switch (base.command) {
-	// 			case "share":
-	// 				let canvas = JSON.parse(base.canvas);
-	// 				console.log(canvas);
-
-	// 				canvas
-	// 					.createPNGStream()
-	// 					.pipe(
-	// 						fs.createWriteStream(path.join(SAVE_DIRECTORY, "shareImg.png"))
-	// 					);
-	// 				// const downloadedAs = FULL_DOWNLOADED_PATH;
-	// 				// await imgToClipboard(downloadedAs);
-
-	// 				fs.unlink(path.join(SAVE_DIRECTORY, "shareImg.png"), (err) => {
-	// 					if (err) {
-	// 						throw err;
-	// 					}
-	// 				});
-	// 				return;
-	// 		}
-	// 	},
-	// 	undefined,
-	// 	context.subscriptions
-	// );
-	// return imgUrl;
 }
 
 function getWebviewContent(
@@ -78,19 +42,15 @@ function getWebviewContent(
 				<title>Cat Coding</title>
 		</head>
 		<body>
-		
 				<canvas id="canvas"></canvas>
 				<script src="${scriptUri}"></script>
 				<script type="module">
 				(function() {
 					const canvas = document.getElementById('canvas');
 					const ctx = canvas.getContext('2d');
-					const vscode = acquireVsCodeApi();
 									
 					phl(canvas, ctx, ${template}, \`${code}\`, "js");		
 
-					// let img = canvas.pngStream();
-					// console.log(canvas.pngStream());
 					setTimeout(() => {
 						let imgURL = canvas.toDataURL(canvas);
 
@@ -99,32 +59,9 @@ function getWebviewContent(
 						a.setAttribute('download', 'shareImg')
 						a.click()
 						document.body.removeChild(a);
-						
-						// vscode.postMessage({
-						// 	command: 'share',
-						// 	img: img
-						// })
-						
 					}, 500);
 				}())
 				</script>
-				
 		</body>
 		</html>`;
 }
-
-// // 2. 周期性改变 html 中的内容，因为是直接给 webview.html 赋值，所以是刷新整个内容
-// function changeWebView() {
-// 	const newData = Math.ceil(Math.random() * 100);
-// 	panel.webview.html = getWebViewContent(`${innerHtml}<p>${newData}</p>`);
-// }
-// const interval = setInterval(changeWebView, 1000);
-
-// // 3. 可以通过设置 panel.onDidDispose，让 webView 在关闭时执行一些清理工作。
-// panel.onDidDispose(
-// 	() => {
-// 		clearInterval(interval);
-// 	},
-// 	null,
-// 	context.subscriptions
-// );
